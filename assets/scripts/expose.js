77279a8567da3f22b918d5338f9e9163b91f5176
bbeeson <bbeeeson@ucsd.edu>
2022-10-30 19:23:03 -0700

    setup

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  
+  let imgsound = document.querySelector('.horn');
+  imgsound.addEventListener('change', function(){
+    const result = document.querySelector('.result');
+    //result.textContent =  `You like ${event.target.value}`;
+    alert('hi');})
+  
 }
+
+/*
+function soundSelect(){
+  const result = document.querySelector('.result');
+  result.textContent =  `You like ${event.target.value}`;
+}
+*/
